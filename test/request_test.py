1f1ec7b0a7263cf4d9c446eff01a3f9a6cf59860
luohua13950 <luohua139@qq.com>
2019-11-01 16:04:31 +0800

    1.bilibili登录测试

--- test/request_test.py
+++ test/request_test.py
@@ -1,26 +1,73 @@
 __author__ = 'luohua139'
-func = lambda x:x
 import re
+import time
+import os
+from PIL import Image
 from urllib import parse
 import urllib.request
-string ="""
-  <link rel="stylesheet" href="//s3plus.meituan.net/v1/mss_e2821d7f0cfe4ac1bf9202ecf9590e67/cdn-prod/file:5788b470/common.0a548310.css"/>
-<link rel="stylesheet" href="//s3plus.meituan.net/v1/mss_e2821d7f0cfe4ac1bf9202ecf9590e67/cdn-prod/file:5788b470/board-index.92a06072.css"/>
-  <script crossorigin="anonymous" src="//s3plus.meituan.net/v1/mss_e2821d7f0cfe4ac1bf9202ecf9590e67/cdn-prod/file:5788b470/stat.88d57c80.js"></script>
-  <script>if(window.devicePixelRatio >= 2) { document.write('<link rel="stylesheet" href="//s3plus.meituan.net/v1/mss_e2821d7f0cfe4ac1bf9202ecf9590e67/cdn-prod/file:5788b470/image-2x.8ba7074d.css"/>') }</script>
-  <style>
-    @font-face {
-      font-family: stonefont;
-      src: url('//vfile.meituan.net/colorstone/9123f6f1971a201181626f573db16e6f3428.eot');
-      src: url('//vfile.meituan.net/colorstone/9123f6f1971a201181626f573db16e6f3428.eot?#iefix') format('embedded-opentype'),
-           url('//vfile.meituan.net/colorstone/4ba02084c75537beea0699d5f40129622280.woff') format('woff');
-    }
-    """
-#ret = re.findall(r"url\((.*?)\)",string)
-# _font_url = re.findall(r"url\((.*?)\)",string)
-# f_url = [url.replace("'","").replace("'","") for url in _font_url]
-# font_url = ["https:"+url for url in f_url if url.endswith("woff")]
-# res = urllib.request.urlretrieve(font_url[0],filename=font_url[0].split("/")[-1])
-srt = "fafa;fafafa;ere;ytytyu;"
-pp = re.sub(";","",srt)
-print(pp)
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+web = webdriver.Chrome(executable_path="..\\data\\webdriver\\chromedriver.exe")
+wait = WebDriverWait(web,timeout=15)
+base = "..\\data\\bilibili\\"
+def login():
+    web.maximize_window()
+    url = "https://passport.bilibili.com/login"
+    username = "abcd"
+    passwd = "123456"
+    web.get(url)
+    user = wait.until(EC.presence_of_element_located((By.ID,"login-username")))
+    password = wait.until(EC.presence_of_element_located((By.ID,"login-passwd")))
+
+    user.send_keys(username)
+    password.send_keys(passwd)
+
+    logn = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"a.btn.btn-login")))
+    time.sleep(2)
+    logn.click()
+    time.sleep(6)
+    dst_pic()
+    web.close()
+
+
+
+def hide_element(element):
+    web.execute_script("arguments[0].style=arguments[1]",element,"display: none;")
+
+def show_element(element):
+    web.execute_script("arguments[0].style=arguments[1]",element,"display: block;")
+
+def save_pic(element,name):
+    web.save_screenshot("..\\data\\bilibili\\bilibili.png")
+    left = element.location["x"]
+    top = element.location["y"]
+    right = left+element.size["width"]
+    bottom = top+element.size["height"]
+    print(left,top)
+    print(right,bottom)
+    im = Image.open("..\\data\\bilibili\\bilibili.png")
+    im = im.crop((left,top,right,bottom))
+    filename = os.path.join(base,name+".png")
+    im.save(filename)
+
+def dst_pic():
+    full_pic_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"canvas.geetest_canvas_fullbg.geetest_fade.geetest_absolute")))
+    slice_pic_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"canvas.geetest_canvas_slice.geetest_absolute")))
+    bg_pic_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"canvas.geetest_canvas_bg.geetest_absolute")))
+    print("隐藏滑块")
+    hide_element(slice_pic_element)
+    save_pic(bg_pic_element,"bg")
+    print("展示滑块")
+    show_element(slice_pic_element)
+    save_pic(slice_pic_element,"slice")
+    print("展示原图")
+    show_element(full_pic_element)
+    save_pic(full_pic_element,"full")
+
+if __name__ == "__main__":
+    login()
+    ActionChains(web).move_by_offset()
